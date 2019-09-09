87860931d3dcc032bfe2a7dc65a924e38ec6cbda
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-09-09 11:54:06 +0100

    chore: switched from 'updatebot' to 'jx step create pr'

---
+++ update-bot.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+jx step create pr docker \
+    --name jenkinsxio/jenkins-filerunner \
+    --version ${VERSION} \
+    --repo https://github.com/jenkins-x/jenkins-x-serverless.git
